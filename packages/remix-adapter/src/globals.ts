569bc20b7463640af23b301978032acdcfff793b
Nick Taylor <nick@iamdeveloper.com>
2023-01-27 10:34:58 -0500

    chore: fixed globals

--- packages/remix-adapter/src/globals.ts
+++ packages/remix-adapter/src/globals.ts
@@ -1,18 +1,2 @@
-/*
-Remix provides `process.env.NODE_ENV` at compile time.
-Declare types for `process` here so that they are available in Deno.
-*/
-
-interface ProcessEnv {
-  NODE_ENV?: "development" | "production" | "test";
-  [key: string]: string | undefined;
-}
-interface Process {
-  env: ProcessEnv;
-}
-declare global {
-  // deno-lint-ignore no-var
-  var process: Process;
-}
-globalThis.process ||= { env: Deno.env.toObject() };
-
+// @ts-ignore
+globalThis.process ||= { env: Deno.env.toObject() }
