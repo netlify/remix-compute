9974faaad1ab7508b4daccb786b54ae7180fa6be
Nick Taylor <nick@iamdeveloper.com>
2023-02-06 11:57:00 -0500

    test: added E2E tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:8888',
+    specPattern: 'cypress/e2e/**/*.spec.ts',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
