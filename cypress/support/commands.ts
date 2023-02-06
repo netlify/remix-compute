7b0970aa4c0dbeac6df2703f0466ecc59a9168b6
Nick Taylor <nick@iamdeveloper.com>
2023-02-06 12:37:32 -0500

    chore: react 18 hydration errors in Cypress handled now

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,39 +1,10 @@
 /// <reference types="cypress" />
 import '@testing-library/cypress/add-commands'
 
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+// Handles React 18 hydration errors
+Cypress.on('uncaught:exception', (err) => {
+  // we check if the error is
+  if (err.message.includes('Minified React error #418;') || err.message.includes('Minified React error #423;')) {
+    return false
+  }
+})
