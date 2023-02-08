daafe3f1bdac2e54a1b900de5dfa58ceb3ad16d8
Nick Taylor <nick@iamdeveloper.com>
2023-02-08 08:42:22 -0500

    chore: set cache-control to 60 seconds for the demo site

--- packages/edge-demo-site/app/routes/index.tsx
+++ packages/edge-demo-site/app/routes/index.tsx
@@ -1,6 +1,8 @@
 export function headers({ loaderHeaders, parentHeaders }: { loaderHeaders: Headers; parentHeaders: Headers }) {
   return {
-    'Cache-Control': 'public, max-age=31536000, s-maxage=31536000',
+    // This is an example of how to set caching headers for a route
+    // For more info on headers in Remix, see: https://remix.run/docs/en/v1/route/headers
+    'Cache-Control': 'public, max-age=60, s-maxage=60',
   }
 }
 
