9ce4b7912c5e7bbc432068cbf9869a0d0203e962
Nick Taylor <nick@iamdeveloper.com>
2023-02-08 07:48:16 -0500

    chore: added a comment about the demo site's server build path

--- packages/edge-demo-site/remix.config.js
+++ packages/edge-demo-site/remix.config.js
@@ -6,9 +6,15 @@ const { config } = require('@netlify/remix-edge-adapter')
  */
 module.exports = {
   ...config,
-  serverBuildPath: '../../.netlify/edge-functions/server.js',
   // This works out of the box with the Netlify adapter, but you can
   // add your own custom config here if you want to.
   //
   // See https://remix.run/docs/en/v1/file-conventions/remix-config
+
+  // The server build path is configure to the root of the monorepo as that's
+  //  where Netlify expects it to be because the base path is the root of the
+  //  monorepo.
+  //
+  // This is not required in user land. It's only for the demo site.
+  serverBuildPath: '../../.netlify/edge-functions/server.js',
 }
