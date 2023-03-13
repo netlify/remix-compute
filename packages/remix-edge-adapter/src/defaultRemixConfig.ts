4aae8cec20f604d6c306245d7b7645b65a36be4d
Nick Taylor <nick@iamdeveloper.com>
2023-03-13 14:35:32 -0400

    feat: moved to Remix routing v2 (#61)

--- packages/remix-edge-adapter/src/defaultRemixConfig.ts
+++ packages/remix-edge-adapter/src/defaultRemixConfig.ts
@@ -7,4 +7,8 @@ export const config: AppConfig = {
   serverBuildPath: '.netlify/edge-functions/server.js',
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
+  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
+  future: {
+    v2_routeConvention: true,
+  },
 }
