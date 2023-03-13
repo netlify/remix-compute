5c7821c9a2f7749923512b1b8f5e850c3b81c708
Nick Taylor <nick@iamdeveloper.com>
2023-03-13 17:29:58 -0400

    feat: moved away from deprecated serverBuildTarget

--- packages/remix-edge-adapter/src/defaultRemixConfig.ts
+++ packages/remix-edge-adapter/src/defaultRemixConfig.ts
@@ -1,10 +1,12 @@
 import type { AppConfig } from '@remix-run/dev'
 
 export const config: AppConfig = {
-  serverBuildTarget: 'deno',
   server: './server.js',
   ignoredRouteFiles: ['**/.*'],
   serverBuildPath: '.netlify/edge-functions/server.js',
+  serverConditions: ['deno', 'worker'],
+  serverDependenciesToBundle: 'all',
+  serverMainFields: ['module', 'main'],
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
   // See https://remix.run/docs/en/main/file-conventions/route-files-v2
