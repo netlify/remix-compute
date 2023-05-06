fecedbf0cd666a02c739ade291b286b678108c1b
Michaël De Boey <info@michaeldeboey.be>
2023-05-06 19:19:45 +0200

    feat(remix-edge-adapter): update default config

--- packages/remix-edge-adapter/src/defaultRemixConfig.ts
+++ packages/remix-edge-adapter/src/defaultRemixConfig.ts
@@ -1,7 +1,7 @@
 import type { AppConfig } from '@remix-run/dev'
 
 export const config: AppConfig = {
-  server: './server.js',
+  server: './server.ts',
   ignoredRouteFiles: ['**/.*'],
   serverBuildPath: '.netlify/edge-functions/server.js',
   serverConditions: ['deno', 'worker'],
@@ -9,8 +9,11 @@ export const config: AppConfig = {
   serverMainFields: ['module', 'main'],
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
-  // See https://remix.run/docs/en/main/file-conventions/route-files-v2
+  // See https://remix.run/pages/v2
   future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
     v2_routeConvention: true,
   },
 }
