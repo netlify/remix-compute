5bbcbc1ad732bcd6ca20dc7699a46a7b832bc7b5
Michaël De Boey <info@michaeldeboey.be>
2023-05-26 22:24:01 +0200

    Update entry.server.tsx

--- packages/edge-demo-site/app/entry.server.tsx
+++ packages/edge-demo-site/app/entry.server.tsx
@@ -6,7 +6,7 @@
 
 import { PassThrough } from 'node:stream'
 
-import type { EntryContext } from '@remix-run/node'
+import type { AppLoadContext, EntryContext } from '@remix-run/node'
 import { Response } from '@remix-run/node'
 import { RemixServer } from '@remix-run/react'
 import isbot from 'isbot'
