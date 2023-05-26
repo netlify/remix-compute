e5478c84501ac38417dec3267c3dbd7a7fa88fee
Michaël De Boey <info@michaeldeboey.be>
2023-05-26 22:03:50 +0200

    Update root.tsx

--- packages/edge-demo-site/app/root.tsx
+++ packages/edge-demo-site/app/root.tsx
@@ -1,16 +1,24 @@
-import type { MetaFunction } from '@remix-run/node'
-import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import { cssBundleHref } from "@remix-run/css-bundle";
+import type { LinksFunction } from "@remix-run/node";
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-})
+export const links: LinksFunction = () => [
+  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
+];
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
@@ -21,5 +29,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  )
+  );
 }
