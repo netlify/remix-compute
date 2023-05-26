388e77307c76f5637e06aee32c2e624b0cef5bb5
Michaël De Boey <info@michaeldeboey.be>
2023-05-26 22:11:21 +0200

    Update server.ts

--- packages/edge-demo-site/server.ts
+++ packages/edge-demo-site/server.ts
@@ -1,14 +1,17 @@
+import { createRequestHandler } from "@netlify/remix-edge-adapter";
 // Import path interpreted by the Remix compiler
-import * as build from '@remix-run/dev/server-build'
-import { createRequestHandler } from '@netlify/remix-edge-adapter'
+import * as build from "@remix-run/dev/server-build";
+import { installGlobals } from "@remix-run/node";
+
+installGlobals();
 
 export default createRequestHandler({
   build,
   // process.env.NODE_ENV is provided by Remix at compile time
   mode: process.env.NODE_ENV,
-})
+});
 
 export const config = {
-  cache: 'manual',
-  path: '/*',
-}
+  cache: "manual",
+  path: "/*",
+};
