580836fcb170a03d712547e50b2361fbbeabd393
Michaël De Boey <info@michaeldeboey.be>
2023-05-26 22:14:55 +0200

    chore: prettier

--- packages/edge-demo-site/app/root.tsx
+++ packages/edge-demo-site/app/root.tsx
@@ -1,17 +1,8 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
-export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : [])]
 
 export default function App() {
   return (
@@ -29,5 +20,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
