6a8e53f0b9a404608ca0becd502c0960071b4023
Michaël De Boey <info@michaeldeboey.be>
2023-06-13 23:36:22 +0200

    update entry.server.tsx

--- packages/edge-demo-site/app/entry.server.tsx
+++ packages/edge-demo-site/app/entry.server.tsx
@@ -1,28 +1,26 @@
-import type { EntryContext } from '@remix-run/server-runtime'
-import { RemixServer } from '@remix-run/react'
-import { renderToReadableStream } from 'react-dom/server'
-import isbot from 'isbot'
+/**
+ * By default, Remix will handle generating the HTTP Response for you.
+ * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
+ * For more information, see https://remix.run/file-conventions/entry.server
+ */
 
-export default async function handleRequest(
+import type { AppLoadContext, EntryContext } from '@remix-run/node'
+import { RemixServer } from '@remix-run/react'
+import { renderToString } from 'react-dom/server'
+
+export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext,
+  loadContext: AppLoadContext,
 ) {
-  const body = await renderToReadableStream(<RemixServer context={remixContext} url={request.url} />, {
-    onError() {
-      responseStatusCode = 500
-    },
-  })
-
-  if (isbot(request.headers.get('user-agent'))) {
-    await body.allReady
-  }
+  const markup = renderToString(<RemixServer context={remixContext} url={request.url} />)
 
   responseHeaders.set('Content-Type', 'text/html')
 
-  return new Response(body, {
-    status: responseStatusCode,
+  return new Response('<!DOCTYPE html>' + markup, {
     headers: responseHeaders,
+    status: responseStatusCode,
   })
 }
