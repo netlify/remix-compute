bed0399bac3d09fc270fcbed482e2288254daf4d
Michaël De Boey <info@michaeldeboey.be>
2023-06-21 15:53:46 +0200

    !feat(remix-edge-adapter): update default config (#84)
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- packages/edge-demo-site/app/root.tsx
+++ packages/edge-demo-site/app/root.tsx
@@ -1,16 +1,15 @@
-import type { MetaFunction } from '@remix-run/node'
+import { cssBundleHref } from '@remix-run/css-bundle'
+import type { LinksFunction } from '@remix-run/node'
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
 
-export const meta: MetaFunction = () => ({
-  charset: 'utf-8',
-  title: 'New Remix App',
-  viewport: 'width=device-width,initial-scale=1',
-})
+export const links: LinksFunction = () => [...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : [])]
 
 export default function App() {
   return (
     <html lang="en">
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
