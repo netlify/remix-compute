1583441ff37620ca0bd48cd0eaeaecb4c5086331
Nick Taylor <nick@iamdeveloper.com>
2023-07-21 14:03:00 -0400

    chore: trying another build integration file copy config #3

--- packages/hydrogen-integration/index.js
+++ packages/hydrogen-integration/index.js
@@ -9,15 +9,10 @@ function onBuild(options) {
     netlifyConfig: { build },
   } = options
   const buildDir = path.join(build.publish, 'build')
-  console.log('build dir', buildDir)
-  console.log('Hydrogen server file', path.join(build.publish, '../dist/worker/index.js'))
-  console.log('client build dir', path.join(build.publish, '../dist/client/build/'))
+  const edgeFunctionsDir = path.join(process.cwd(), INTERNAL_EDGE_FUNCTIONS_SRC)
 
-  fs.mkdirSync(INTERNAL_EDGE_FUNCTIONS_SRC, { recursive: true })
-  fs.copyFileSync(
-    path.join(build.publish, '../dist/worker/index.js'),
-    path.join(INTERNAL_EDGE_FUNCTIONS_SRC, 'server.js'),
-  )
+  fs.mkdirSync(edgeFunctionsDir, { recursive: true })
+  fs.copyFileSync(path.join(build.publish, '../dist/worker/index.js'), path.join(edgeFunctionsDir, 'server.js'))
 
   fs.mkdirSync(buildDir, { recursive: true })
   fs.cpSync(path.join(build.publish, '../dist/client/build/'), buildDir, { recursive: true })
