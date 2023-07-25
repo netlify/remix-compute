7fff63f32b3ee0f97d5e3a38bb83b198d3cd9403
Nick Taylor <nick@iamdeveloper.com>
2023-07-25 16:15:18 -0400

    chore: added workaround for local deploy/serve vs/ git push deploy

--- packages/hydrogen-integration/index.js
+++ packages/hydrogen-integration/index.js
@@ -8,14 +8,20 @@ function onBuild(options) {
     constants: { INTERNAL_EDGE_FUNCTIONS_SRC },
     netlifyConfig: { build },
   } = options
-  const buildDir = path.join(build.publish, 'build')
+  const buildDir = 'build'
+  // Check to see if the path exists. Currently with the monorepo support, the current
+  // working directory is different than when a deploy runs via git push.
+  // e.g. local build cwd: /Users/u/repos/remix-compute/packages/hydrogen-demo-site/packages/hydrogen-demo-site/public
+  // vs.
+  // e.g. deploy cwd: /opt/build/repo/packages/hydrogen-demo-site/public
+  const publishDir = fs.existsSync(build.publish) ? build.publish : 'public'
   const edgeFunctionsDir = path.join(process.cwd(), INTERNAL_EDGE_FUNCTIONS_SRC)
 
   fs.mkdirSync(edgeFunctionsDir, { recursive: true })
-  fs.copyFileSync(path.join(build.publish, '../dist/worker/index.js'), path.join(edgeFunctionsDir, 'server.js'))
+  fs.copyFileSync(path.join(publishDir, '../dist/worker/index.js'), path.join(edgeFunctionsDir, 'server.js'))
 
   fs.mkdirSync(buildDir, { recursive: true })
-  fs.cpSync(path.join(build.publish, '../dist/client/build/'), buildDir, { recursive: true })
+  fs.cpSync(path.join(publishDir, '../dist/client/build/'), buildDir, { recursive: true })
 
   // eslint-disable-next-line no-console
   console.log(
