f7d6c158ac91bb68e9403755162ea09bbec27313
Nick Taylor <nick@iamdeveloper.com>
2023-07-25 11:57:32 -0400

    chore: move edge function inline to demo site server.js

--- packages/hydrogen-demo-site/server.js
+++ packages/hydrogen-demo-site/server.js
@@ -1,7 +1,4 @@
-import {
-  generateHandlerFunction,
-  netlifyEdgeConfig,
-} from '@netlify/hydrogen-edge-adapter';
+import {generateHandlerFunction} from '@netlify/hydrogen-edge-adapter';
 
 // Project-specific imports
 import {HydrogenSession} from '~/lib/session.server';
@@ -12,5 +9,17 @@ export default generateHandlerFunction({
   getLocaleFromRequest,
 });
 
-// Netlify Edge Function configuration
-export {netlifyEdgeConfig as config};
+export const config = {
+  path: '/*',
+
+  // Allow Remix to handle caching Edge Functions
+  // https://docs.netlify.com/edge-functions/optional-configuration/#configure-an-edge-function-for-caching
+  cache: 'manual',
+
+  // Let the CDN handle requests for static assets, i.e. /_assets/*
+  // Add other exclusions here, e.g. "/api/*$" for custom Netlify functions or custom Netlify Edge Functions
+  excludedPath: ['/build/*'],
+
+  // Use built-in Remix error handling
+  onError: 'bypass',
+};
