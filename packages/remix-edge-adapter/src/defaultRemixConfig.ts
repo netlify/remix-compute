2781e73e36efdaef51e6daee86cb9cabcade4ea1
Matt Kane <matt.kane@netlify.com>
2023-10-16 21:10:59 +0100

    feat!: upgrade to Remix 2

--- packages/remix-edge-adapter/src/defaultRemixConfig.ts
+++ packages/remix-edge-adapter/src/defaultRemixConfig.ts
@@ -1,7 +1,7 @@
 import type { AppConfig } from '@remix-run/dev'
 
 export const config: AppConfig = {
-  server: './server.ts',
+  server: './server.js',
   ignoredRouteFiles: ['**/.*'],
   serverBuildPath: '.netlify/edge-functions/server.js',
   serverConditions: ['deno', 'worker'],
@@ -9,13 +9,4 @@ export const config: AppConfig = {
   serverMainFields: ['module', 'main'],
   serverModuleFormat: 'esm',
   serverPlatform: 'neutral',
-  // See https://remix.run/pages/v2
-  future: {
-    v2_dev: true,
-    v2_errorBoundary: true,
-    v2_headers: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-  },
 }
