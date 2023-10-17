6d6a7f9bb821b256589e42311318dd0fcec2ca73
ascorbic <ascorbic@gmail.com>
2023-10-17 19:20:16 +0100

    chore: re-enable

--- packages/demo-site/server.ts
+++ packages/demo-site/server.ts
@@ -1,16 +1,15 @@
-// import * as build from '@remix-run/dev/server-build'
-// import { createRequestHandler } from '@netlify/remix-adapter'
+import * as build from '@remix-run/dev/server-build'
+import { createRequestHandler } from '@netlify/remix-adapter'
 import type { Context, Config } from '@netlify/functions'
 
-// const handle = createRequestHandler({
-//   build,
-//   mode: process.env.NODE_ENV,
-// })
+const handle = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+})
 
 export default function handler(request: Request, context: Context) {
   console.log('handler')
-  return new Response('hello world')
-  // return handle(request, context)
+  return handle(request, context)
 }
 
 export const config: Config = {
