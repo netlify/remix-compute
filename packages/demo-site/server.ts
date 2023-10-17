f9187b110b7f4960ecef4d3c60aa0e56bfbdef71
ascorbic <ascorbic@gmail.com>
2023-10-17 19:25:31 +0100

    chore: try with a different response

--- packages/demo-site/server.ts
+++ packages/demo-site/server.ts
@@ -2,14 +2,16 @@ import * as build from '@remix-run/dev/server-build'
 import { createRequestHandler } from '@netlify/remix-adapter'
 import type { Context, Config } from '@netlify/functions'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const handle = createRequestHandler({
   build,
   mode: process.env.NODE_ENV,
 })
 
-export default function handler(request: Request, context: Context) {
+export default async function handler(request: Request, context: Context) {
   console.log('handler')
-  return handle(request, context)
+  return await fetch('https://www.netlify.com/favicon.ico')
+  // return handle(request, context)
 }
 
 export const config: Config = {
