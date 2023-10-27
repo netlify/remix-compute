d117ddff7f333762f52cd78b35fb79569764816c
Matt Kane <matt.kane@netlify.com>
2023-10-27 17:24:44 +0100

    feat: export the default 'handleRequest' from adapters

--- packages/remix-adapter/src/index.ts
+++ packages/remix-adapter/src/index.ts
@@ -1,3 +1,4 @@
 export { config } from './defaultRemixConfig'
 export type { GetLoadContextFunction, RequestHandler } from './server'
 export { createRequestHandler } from './server'
+export { handleRequest } from './entry.server'
