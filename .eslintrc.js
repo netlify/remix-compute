4a7d5555bcc150048bef8dfc695af3cd0664832c
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-07 15:33:23 -0400

    chore: port e2e tests from Cypress to Playwright

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'plugin:playwright/recommended'],
   plugins: ['@typescript-eslint'],
   parserOptions: {
     sourceType: 'module',
