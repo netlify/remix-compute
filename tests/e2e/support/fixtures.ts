5bfac4f110cd1874fe23c2c6ac12b1cc663edf8e
Philippe Serhal <philippe.serhal@netlify.com>
2024-05-15 17:29:24 -0400

    test: add e2e fixtures for Remix Vite sites

--- tests/e2e/support/fixtures.ts
+++ tests/e2e/support/fixtures.ts
@@ -3,8 +3,16 @@ import { test as base } from '@playwright/test'
 import { type Fixture, deployFixture } from './deploy-to-netlify'
 
 /* eslint-disable-next-line @typescript-eslint/no-empty-interface */
-interface TestFixtures { }
+interface TestFixtures {}
 interface WorkerFixtures {
+  /**
+   * A (Vite) Remix site using origin SSR
+   */
+  serverlessSite: Fixture
+  /**
+   * A (Vite) Remix site using edge SSR
+   */
+  edgeSite: Fixture
   /**
    * A "classic" (non-Vite) Remix site using origin SSR
    */
@@ -16,15 +24,29 @@ interface WorkerFixtures {
 }
 
 export const test = base.extend<TestFixtures, WorkerFixtures>({
+  serverlessSite: [
+    async ({}, use) => {
+      const fixture = await deployFixture('serverless-site')
+      await use(fixture)
+    },
+    { scope: 'worker' },
+  ],
+  edgeSite: [
+    async ({}, use) => {
+      const fixture = await deployFixture('edge-site')
+      await use(fixture)
+    },
+    { scope: 'worker' },
+  ],
   classicServerlessSite: [
-    async ({ }, use) => {
+    async ({}, use) => {
       const fixture = await deployFixture('classic-serverless-site')
       await use(fixture)
     },
     { scope: 'worker' },
   ],
   classicEdgeSite: [
-    async ({ }, use) => {
+    async ({}, use) => {
       const fixture = await deployFixture('classic-edge-site')
       await use(fixture)
     },
--- tests/e2e/user-journeys.spec.ts
+++ tests/e2e/user-journeys.spec.ts
@@ -1,6 +1,18 @@
 import { expect, test } from './support/fixtures'
 
 test.describe('User journeys', () => {
+  test('serves a response from the origin when using @netlify/remix-adapter', async ({ page, serverlessSite }) => {
+    const response = await page.goto(serverlessSite.url)
+    await expect(page.getByRole('heading', { name: /Welcome to Remix/i })).toBeVisible()
+    expect(response?.headers()['x-nf-function-type']).toBe('request')
+  })
+
+  test('serves a response from the edge when using @netlify/remix-edge-adapter', async ({ page, edgeSite }) => {
+    const response = await page.goto(edgeSite.url)
+    await expect(page.getByRole('heading', { name: /Welcome to Remix/i })).toBeVisible()
+    expect(response?.headers()['x-nf-edge-functions']).toBe('remix-server')
+  })
+
   test.describe('classic Remix compiler', () => {
     test('serves a response from the origin when using @netlify/remix-adapter', async ({
       page,
