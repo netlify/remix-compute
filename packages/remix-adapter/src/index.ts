ad70982594a2b7a850f4e31de5bd5023d45bab36
Philippe Serhal <philippe.serhal@netlify.com>
2024-06-28 08:45:37 -0400

    refactor(remix-adapter): move entry.server to non-Vite
    This file is actually not used by Vite sites. I was misled because the
    same file *is* used by Vite sites in the case of remix-edge-adapter.

--- packages/remix-adapter/src/index.ts
+++ packages/remix-adapter/src/index.ts
@@ -1,4 +1,4 @@
 export { config } from './classic-compiler/defaultRemixConfig'
+export { handleRequest } from './classic-compiler/entry.server'
 export type { GetLoadContextFunction, RequestHandler } from './common/server'
 export { createRequestHandler } from './common/server'
-export { handleRequest } from './common/entry.server'
