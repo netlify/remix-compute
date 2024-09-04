080e7bc763c6d5f8e3618973ed9fb10796b9979b
Philippe Serhal <philippe.serhal@netlify.com>
2024-09-04 17:08:06 -0400

    fix: fix Hydrogen types

--- packages/remix-edge-adapter/src/vite/hydrogen.ts
+++ packages/remix-edge-adapter/src/vite/hydrogen.ts
@@ -1,6 +1,4 @@
 import type { Context } from '@netlify/edge-functions'
-import type { AppLoadContext } from '@netlify/remix-runtime'
-import type { HydrogenEnv } from '@shopify/hydrogen'
 
 /**
  * The base Hydrogen templates expect a globally defined `ExecutionContext` type, which by default
@@ -17,21 +15,19 @@ declare global {
 /**
  * For convenience, this matches the function signature that Hydrogen includes by default in its templates:
  * https://github.com/Shopify/hydrogen/blob/92a53c477540ee22cc273e7f3cbd2fd0582c815f/templates/skeleton/app/lib/context.ts.
- */
-type CreateAppLoadContext = (
-  request: Request,
-  env: HydrogenEnv,
-  executionContext: ExecutionContext,
-) => Promise<AppLoadContext>
-/**
- * You might think the return type above and other types in this file should have a type parameter
- * so that user-defined context and Hydrogen context could be merged with the Netlify context, but
- * this isn't the pattern Remix uses. Instead, Remix expects the user to use module augmentation to
- * modify the exported `AppLoadContext` type. See
+ *
+ * Remix expects the user to use module augmentation to modify their exported `AppLoadContext` type. See
  * https://github.com/remix-run/remix/blob/5dc3b67dc31f3df7b1b0298ae4e9cac9c5ae1c06/packages/remix-server-runtime/data.ts#L15-L23
+ * Hydrogen follows this pattern. However, because of the way TypeScript module augmentation works,
+ * we can't access the final user-augmented type here, so we have to do this dance with generic types.
  */
+type CreateAppLoadContext<E extends {}, C extends {}> = (
+  request: Request,
+  env: E,
+  executionContext: ExecutionContext,
+) => Promise<C>
 
-const executionContext = {
+const executionContext: ExecutionContext = {
   /**
    * Hydrogen expects a `waitUntil` function like the one in the workerd runtime:
    * https://developers.cloudflare.com/workers/runtime-apis/context/#waituntil.
@@ -54,11 +50,15 @@ const getEnv = () => {
   return process.env
 }
 
-export const createHydrogenAppLoadContext = async (
+export const createHydrogenAppLoadContext = async <E extends {}, C extends {}>(
   request: Request,
   netlifyContext: Context,
-  createAppLoadContext: CreateAppLoadContext,
-): Promise<AppLoadContext> => {
-  // NOTE: We use `Object.assign` here to avoid TODO
-  return Object.assign(netlifyContext, await createAppLoadContext(request, getEnv(), executionContext))
+  createAppLoadContext: CreateAppLoadContext<E, C>,
+): Promise<Context & C & Record<string, unknown>> => {
+  const env = getEnv() as E
+  const userHydrogenContext = await createAppLoadContext(request, env, executionContext)
+
+  // NOTE: We use `Object.assign` here because a spread would access the getters on the
+  // `netlifyContext` fields, some of which throw a "not implemented" error in local dev.
+  return Object.assign(netlifyContext, userHydrogenContext)
 }
