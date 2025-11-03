f72691475b55c47142d372c988aae1c9ded053fe
Philippe Serhal <philippe.serhal@gmail.com>
2025-11-03 15:18:16 -0500

    build: allow code ESM splitting
    By declaring the ESM entries together, we allow code splitting, which
    keeps the size of our package to a minimum.

--- packages/vite-plugin-react-router/tsup.config.ts
+++ packages/vite-plugin-react-router/tsup.config.ts
@@ -2,15 +2,20 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig([
   {
-    entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    entry: {
+      index: 'src/index.ts',
+      serverless: 'src/runtimes/netlify-functions.ts',
+      edge: 'src/runtimes/netlify-edge-functions.ts',
+    },
+    format: ['esm'],
     dts: true,
     target: 'node18',
     clean: true,
   },
+  // TODO(serhalp): Remove in a future major version.
   {
-    entry: { serverless: 'src/runtimes/netlify-functions.ts', edge: 'src/runtimes/netlify-edge-functions.ts' },
-    format: ['esm'],
+    entry: ['src/index.ts'],
+    format: ['cjs'],
     dts: true,
     target: 'node18',
   },
