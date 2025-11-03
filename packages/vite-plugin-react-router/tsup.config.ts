eb018ee0d776dad8b16ed1ecd3811801aab7d876
Philippe Serhal <philippe.serhal@gmail.com>
2025-11-03 17:02:18 -0500

    build: fix build setup regression

--- packages/vite-plugin-react-router/tsup.config.ts
+++ packages/vite-plugin-react-router/tsup.config.ts
@@ -10,7 +10,9 @@ export default defineConfig([
     format: ['esm'],
     dts: true,
     target: 'node18',
-    clean: true,
+    // Can't use `clean: true` because of this bug: https://github.com/egoist/tsup/issues/670.
+    // TODO(serhalp): Switch to tsdown; tsup is no longer maintained anyway.
+    clean: false,
   },
   // TODO(serhalp): Remove in a future major version.
   {
@@ -18,5 +20,6 @@ export default defineConfig([
     format: ['cjs'],
     dts: true,
     target: 'node18',
+    clean: false,
   },
 ])
