88b44ae2594e31caddebce392ab33584c3e32f6e
Philippe Serhal <philippe.serhal@netlify.com>
2025-11-05 04:21:36 -0500

    test: remove or simplify unreliable CDN caching e2e tests (#563)
    * test: remove on-demand revalidation e2e tests
    
    These are absolutely not worth their maintenance cost.
    
    I've had enough.
    
    * test: skip test for broken behaviour
    
    * test: simplify cached response e2e tests
    
    These are unreliable as written, and simply not worth their maintenance
    cost.

--- tests/e2e/fixtures/react-router-serverless-site/app/routes/cacheable.tsx
+++ tests/e2e/fixtures/react-router-serverless-site/app/routes/cacheable.tsx
@@ -7,8 +7,7 @@ export const loader = () => {
     },
     {
       headers: {
-        'CDN-Cache-Control': 'public, max-age=31536000, durable',
-        'Cache-Tag': 'cached-for-a-year-tag',
+        'CDN-Cache-Control': 'public, max-age=60, durable',
       },
     },
   )
@@ -18,11 +17,11 @@ export const headers: HeadersFunction = ({ loaderHeaders }) => {
   return loaderHeaders
 }
 
-export default function CachedForAYearDemo() {
+export default function SWRDemo() {
   const { message } = useLoaderData<typeof loader>()
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Cached for a year</h1>
+      <h1>Cacheable</h1>
       <p>{message}</p>
     </div>
   )
