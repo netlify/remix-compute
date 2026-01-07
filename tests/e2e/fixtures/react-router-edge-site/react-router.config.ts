c04f2cdfe99b3af6f24facc218a713dc92f4d8fa
나형진 <skgudwls@konkuk.ac.kr>
2026-01-07 19:10:04 +0900

    test: enable prerender e2e tests after rollupOptions.input fix
    - Enable previously skipped prerender tests for both serverless and edge sites.
    - The tests were disabled due to React Router issue #13226, but now work correctly after the rollupOptions.input merge fix in plugin.ts.

--- tests/e2e/fixtures/react-router-edge-site/react-router.config.ts
+++ tests/e2e/fixtures/react-router-edge-site/react-router.config.ts
@@ -4,7 +4,8 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  // TODO(serhalp) Revisit this if RR team changes their minds:
-  // https://github.com/remix-run/react-router/issues/13226#issuecomment-2776672461.
-  // prerender: ['/prerendered'],
+  // Previously disabled due to https://github.com/remix-run/react-router/issues/13226#issuecomment-2776672461.
+  // Now enabled after fixing rollupOptions.input merge in plugin.ts to preserve existing entries
+  // including 'virtual:react-router/server-build'.
+  prerender: ['/prerendered'],
 } satisfies Config
